22ec6756a9e74f1120a2ebdf57e55bc5bd871881
calvinpark <dev.calvinpark@gmail.com>
2022-12-29 17:24:52 +0900

    feat: set y coordinate

--- Pong1.py
+++ Pong1.py
@@ -24,9 +24,6 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-
-
-    
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
@@ -35,12 +32,13 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
-
 # Function
 def paddle_a_up():
-    
-
-
+    # it returns the y coordinate
+    y = paddle_a.ycorn()
+    # add 20 to y pixel
+    y += 20
+    paddle_a.sety(y)
 
 
 # Main game loop
