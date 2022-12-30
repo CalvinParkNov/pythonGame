6c190636f695df8d4ce661b3139b9945df945758
calvinpark <dev.calvinpark@gmail.com>
2022-12-30 16:09:30 +0900

    feat: score completed

--- Pong1.py
+++ Pong1.py
@@ -6,6 +6,12 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer()
 
+
+# Score
+score_a = 0
+score_b = 0
+
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -33,6 +39,18 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx=2
 ball.dy=-2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+# pen.write("Player A : 0 Player B : 0", align = "center", font =("Courier", 24, "normal"))
+
+
+
 
 # paddle_a_up Function
 def paddle_a_up():
@@ -94,10 +112,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 100
+        pen.clear()
+        pen.write("Player A : {} Player B : {}".format(score_a, score_b), align = "center", font =("Courier", 24, "normal"))
+
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 100
+        pen.clear()
+        pen.write("Player A : {} Player B : {}".format(score_a, score_b), align = "center", font =("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350)and (ball.ycor()< paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 40):
